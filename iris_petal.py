6954455c11a57909ffb8c18ba5573e50a0160297
Zhenqi <chaizhenqi@outlook.com>
2019-05-16 11:04:04 +0800

    add codes

---
+++ iris_petal.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+
+iris = datasets.load_iris()
+x = iris.data[:,2] # X-Axis - petal length
+y = iris.data[:,3] # Y-Axis - petal width
+
+species = iris.target # Species
+
+x_min, x_max = x.min() - .5, x.max() + .5
+y_min, y_max = y.min() - .5, y.max() + .5
+
+plt.figure()
+plt.title('Iris Dataset - Classification By Petal Sizes', size=14)
+plt.scatter(x,y, c=species)
+plt.xlabel('Petal length')
+plt.ylabel('Petal width')
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
